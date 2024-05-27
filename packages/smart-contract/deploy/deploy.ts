89be1a42563cbaba441b842fc45f75adcb18c825
vinhyenvodoi98 <vinhyenvodoi98@gmail.com>
2024-05-28 00:41:03 +0900

    add baseSepolia

--- packages/smart-contract/deploy/deploy.ts
+++ packages/smart-contract/deploy/deploy.ts
@@ -1,9 +1,43 @@
-import { deployContract } from "./utils";
+import { ethers, network } from "hardhat";
+import fs from "fs";
+require('dotenv').config()
 
-// An example of a basic deploy script
-// It will deploy a Greeter contract to selected network
-// as well as verify it on Block Explorer if possible for the network
-export default async function () {
-  const contractArtifactName = "BoardGame";
-  await deployContract(contractArtifactName);
+async function main() {
+  const board = await ethers.deployContract("BoardGame", [], {}) as any;
+
+  await board.waitForDeployment();
+
+  console.log(
+    `deployed to ${board.target}`
+  );
+
+  const contractAddresses = readDataFromFile();
+
+  if (contractAddresses[network.config.chainId as number]) {
+    contractAddresses[network.config.chainId as number].address = board.target;
+  } else {
+    contractAddresses[network.config.chainId as number] = { address: board.target };
+  }
+  // Save the updated array to the JSON file
+  writeDataToFile(contractAddresses);
 }
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
+
+const readDataFromFile = () => {
+  try {
+    const data = fs.readFileSync('contract-address.json', 'utf8');
+    return JSON.parse(data);
+  } catch (err) {
+    return {};
+  }
+};
+
+const writeDataToFile = (data : any) => {
+  fs.writeFileSync('contract-address.json', JSON.stringify(data, null, 2));
+};
--- packages/smart-contract/hardhat.config.ts
+++ packages/smart-contract/hardhat.config.ts
@@ -1,6 +1,9 @@
 import { HardhatUserConfig } from "hardhat/config";
-
+import "@nomicfoundation/hardhat-toolbox";
 import "@matterlabs/hardhat-zksync";
+require('dotenv').config()
+
+const { WALLET_PRIVATE_KEY } = process.env;
 
 const config: HardhatUserConfig = {
   defaultNetwork: "zkSyncSepoliaTestnet",
@@ -17,6 +20,11 @@ const config: HardhatUserConfig = {
     //   zksync: true,
     //   verifyURL: "https://zksync2-mainnet-explorer.zksync.io/contract_verification",
     // },
+    baseSepolia: {
+      url: "https://sepolia.base.org",
+      accounts: [WALLET_PRIVATE_KEY as string],
+      chainId: 84532,
+    },
     hardhat: {
       zksync: true,
     },
